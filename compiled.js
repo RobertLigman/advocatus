2e5737caaaea2b071567d7c88c0bbd6cfbc6e317
Robert Ligman <verxus123@gmail.com>
2019-04-17 11:11:49 +0200

    deleted react elements

--- compiled.js
+++
@@ -1,4 +0,0 @@
-let lol = React.createElement("h1", {
-  className: "big-font"
-}, "dhfdfsu");
-ReactDOM.render(lol, document.getElementById('app'));
--- js.js
+++
@@ -1,5 +0,0 @@
-import './styles/style.css';
-let lol= <h1 className='big-font'>dhfdfsu</h1>;
-ReactDOM.render(lol,document.getElementById('app'));
-
-
